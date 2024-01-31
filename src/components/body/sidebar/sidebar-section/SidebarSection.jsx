7221cb4442211d51cae74e8b7096e1c6ece54c68
Ruban Sahoo <rubansahoo1@gmail.com>
2024-01-31 20:05:51 +0530

    updated sidebar component

--- src/components/body/sidebar/sidebar-section/SidebarSection.jsx
+++ src/components/body/sidebar/sidebar-section/SidebarSection.jsx
@@ -1,15 +1,50 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const SidebarItem = ({ icon, title }) => {
+  const darkTheme = useSelector((store) => store.app.darkTheme);
+  return (
+    <span className="my-2 flex">
+      <img
+        className={
+          "w-6 rounded-md" + (darkTheme ? " bg-darkModeLightGray" : " bg-white")
+        }
+        src={icon}
+        alt="icon"
+      />
+      <span className="pl-5">{title}</span>
+    </span>
+  );
+};
 
 const SidebarSection = ({ heading, tabs }) => {
+  const darkTheme = useSelector((store) => store.app.darkTheme);
   return (
     <>
-      {heading !== "" && <h1 className="font-bold pt-5">{heading}</h1>}
+      {heading !== "" && <h1 className="font-bold text-xl">{heading}</h1>}
       {tabs.length > 0 && (
-        <ul>
-          {tabs.map((tab, index) => (
-            <li key={index}>{tab}</li>
-          ))}
-        </ul>
+        <div>
+          <ul>
+            {tabs.map((tab, index) => (
+              <li key={index}>
+                {tab?.link ? (
+                  <Link to={tab?.link}>
+                    <SidebarItem {...tab} />
+                  </Link>
+                ) : (
+                  <SidebarItem {...tab} />
+                )}
+              </li>
+            ))}
+          </ul>
+          <hr
+            className={
+              "border-t my-4" +
+              (darkTheme ? " border-darkModeGray" : "  border-gray-300")
+            }
+          />
+        </div>
       )}
     </>
   );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,17 +1,57 @@
-import { Link } from "react-router-dom";
+import homeOutlined from "../assets/homeOutlined.svg";
+import shortsOutlined from "../assets/shortsOutlined.svg";
+import subscriptionsOutlined from "../assets/subscriptionsOutlined.svg";
+import library from "../assets/library.svg";
+import yourChannel from "../assets/yourChannel.svg";
+import history from "../assets/history.svg";
+import yourVideos from "../assets/yourVideos.svg";
+import watchLater from "../assets/watchLater.svg";
+import trending from "../assets/trending.svg";
+import shopping from "../assets/shopping.svg";
+import music from "../assets/music.svg";
+import movies from "../assets/movies.svg";
+import live from "../assets/live.svg";
+import gaming from "../assets/gaming.svg";
+import news from "../assets/news.svg";
+import sports from "../assets/sports.svg";
+import learning from "../assets/learning.svg";
+import fashion from "../assets/fashion.svg";
+import podcasts from "../assets/podcasts.svg";
 
 const SECTION_DATA = [
   {
     heading: "",
-    tabs: [<Link to="/">Home</Link>, "Shorts", "Videos", "Live"],
+    tabs: [
+      { id: 1, title: "Home", icon: homeOutlined, link: "/" },
+      { id: 2, title: "Shorts", icon: shortsOutlined },
+      { id: 3, title: "Subscriptions", icon: subscriptionsOutlined },
+      { id: 4, title: "Library", icon: library },
+    ],
   },
   {
-    heading: "Subscriptions",
-    tabs: ["Music", "Sports", "Gaming", "Movies"],
+    heading: "",
+    tabs: [
+      { id: 5, title: "Your channel", icon: yourChannel },
+      { id: 6, title: "History", icon: history },
+      { id: 7, title: "Your videos", icon: yourVideos },
+      { id: 8, title: "Watch later", icon: watchLater },
+    ],
   },
   {
-    heading: "Watch Later",
-    tabs: ["Music", "Sports", "Gaming", "Movies"],
+    heading: "Explore",
+    tabs: [
+      { id: 9, title: "Trending", icon: trending },
+      { id: 10, title: "Shopping", icon: shopping },
+      { id: 11, title: "Music", icon: music },
+      { id: 12, title: "Movies", icon: movies },
+      { id: 13, title: "Live", icon: live },
+      { id: 14, title: "Gaming", icon: gaming },
+      { id: 15, title: "News", icon: news },
+      { id: 16, title: "Sports", icon: sports },
+      { id: 17, title: "Learning", icon: learning },
+      { id: 18, title: "Fashion & Beauty", icon: fashion },
+      { id: 19, title: "Podcasts", icon: podcasts },
+    ],
   },
 ];
 
